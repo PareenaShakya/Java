678524047856b6a2b037a7795f725cb9e97e0f86
Pareena Shakya <pareenashakya@gmail.com>
2025-11-20 11:50:16 +0545

    Done

--- DataTypeInspector.java
+++ DataTypeInspector.java
@@ -2,12 +2,27 @@
 /**
  * Write a description of class DataTypeInspector here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Pareena)
+ * @version (v0.1)
  */
 public class DataTypeInspector
 {
     public static void main(String[] args){
+        int a=5;
+        byte b=10;
+        short c=15;
+        long d=999999999L;
+        float f1=12.5f;
+        double d1=99.99;
+        char ch='A';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(ch);
+        
         
     }
 }
