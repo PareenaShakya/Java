481e083aedf96b191d1ee387b7566d3e032e7fdc
Pareena Shakya <pareenashakya@gmail.com>
2025-11-20 17:27:41 +0545

    Done

---
+++ LiteralPractice.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class LiteralPractice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LiteralPractice
+{
+    public static void main(String[] args) {
+
+        long bigNumber = 9876543210L;
+
+        float decimalValue = 45.67f;
+
+    
+        char copyrightSymbol = '\u00A9';
+
+        System.out.println("Long literal value: " + bigNumber);
+        System.out.println("Float literal value: " + decimalValue);
+        System.out.println("Unicode char value: " + copyrightSymbol);
+    }
+}
