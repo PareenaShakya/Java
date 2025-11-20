6d46cd33b91d69d32db52d6ead225dcc2cb138e3
Pareena Shakya <pareenashakya@gmail.com>
2025-11-20 10:31:03 +0545

    This is ww.

---
+++ WW.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class WW here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WW
+{
+    public static void main(String[] args){
+        
+    }
+}
